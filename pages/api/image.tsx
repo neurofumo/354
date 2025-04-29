55c84f55851e7f40f7f56353e666e2c8a4a4b322
neurofumo <jasonramesdames@gmail.com>
2025-04-30 00:50:07 +0800

    lol

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -42,8 +42,8 @@ export default async function handler(req: NextRequest) {
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 626,
+      height: 351,
     }
   );
 }
